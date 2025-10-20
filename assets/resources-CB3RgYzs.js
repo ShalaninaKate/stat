76abe65f5a46c1e9ca53cb3867a7935d836d36d3
ShalaninaKate <shalaninakate@gmail.com>
2025-10-20 22:32:02 +0300

    Updates

--- assets/resources-CB3RgYzs.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BX8tQDhQ.js";function n(){return o.jsx("div",{children:'Hello "/_mainLayout/resources"!'})}export{n as component};
---
+++ assets/resources-D5FIbQmI.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BplGWkEC.js";function n(){return o.jsx("div",{children:'Hello "/_mainLayout/resources"!'})}export{n as component};
